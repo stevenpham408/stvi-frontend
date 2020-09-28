d4c6ed2d71fd405bf5d83789f11e3e44c0178ba3
Steven Pham <stevenpham408@yahoo.com>
2020-09-27 23:23:30 -0700

    Removed react-typing-effect dependency (faulty)

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,49 +1,20 @@
 import React from 'react'
-import ReactTypingEffect from 'react-typing-effect'
-import {styled} from 'styled-components'
+import styled from 'styled-components'
 
+const TypeWriterStyles = styled.div`
+  color: #bb86fc;
+  font-size: 30px;
+  font-family: "Nunito Sans", sans-serif;
+  #TypingAnimationBlock {
+    /* position styles here */
+  }
+`;
 
-const TypeWriterAnimation: React.FC = () => {
-    return(
-        <div>
-            <ReactTypingEffect
-                speed = "100"
-                text = "shorter."
-                typingDelay = "0"
-                eraseDelay = "16000"
-            />
-            <br/>
-            <ReactTypingEffect
-                speed = "100"
-                text = "sleeker."
-                typingDelay = "4000"
-                eraseDelay = "12000"
-            />
-            <br/>
-            <ReactTypingEffect
-                speed = "100"
-                text = "smarter."
-                typingDelay = "8000"
-                eraseDelay = "8000"
-            />
-            <br/>
-            <ReactTypingEffect
-                speed = "100"
-                text = "stvi."
-                typingDelay = "12000"
-                eraseDelay = "4600"
-            />
-        </div>
-
-    );
-
-} 
 
 const LandingPage: React.FC = () => {
 
     return(
         <div>
-            <TypeWriterAnimation/>
         </div>
     );
 }
