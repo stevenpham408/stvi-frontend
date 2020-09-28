8dc6a04f55c872c44704c5a1663b69f8ee733e9c
Steven Pham <stevenpham408@yahoo.com>
2020-09-27 21:49:22 -0700

    Created component that represents Landing Page

---
+++ src/Components/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {styled} from 'styled-components'
+
+
+
+const LandingPage: React.FC = () => {
+
+    return(
+        <div>
+            Hello
+        </div>
+
+
+    );
+}
