f75fdf5f531cbed5daf7e16aee6f7aa822901cb5
Steven Pham <stevenpham408@yahoo.com>
2020-09-27 22:33:55 -0700

    Added type writer animation for landing page

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,15 +1,51 @@
 import React from 'react'
+import ReactTypingEffect from 'react-typing-effect'
 import {styled} from 'styled-components'
 
 
+const TypeWriterAnimation: React.FC = () => {
+    return(
+        <div>
+            <ReactTypingEffect
+                speed = "100"
+                text = "shorter."
+                typingDelay = "0"
+                eraseDelay = "16000"
+            />
+            <br/>
+            <ReactTypingEffect
+                speed = "100"
+                text = "sleeker."
+                typingDelay = "4000"
+                eraseDelay = "12000"
+            />
+            <br/>
+            <ReactTypingEffect
+                speed = "100"
+                text = "smarter."
+                typingDelay = "8000"
+                eraseDelay = "8000"
+            />
+            <br/>
+            <ReactTypingEffect
+                speed = "100"
+                text = "stvi."
+                typingDelay = "12000"
+                eraseDelay = "4600"
+            />
+        </div>
+
+    );
+
+} 
 
 const LandingPage: React.FC = () => {
 
     return(
         <div>
-            Hello
+            <TypeWriterAnimation/>
         </div>
-
-
     );
 }
+
+export default LandingPage;
