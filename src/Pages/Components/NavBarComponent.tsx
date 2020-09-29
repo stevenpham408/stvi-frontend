d29affc32f3b47cbfc04a74db08ce90cc789b324
Steven Pham <stevenpham408@yahoo.com>
2020-09-28 17:47:10 -0700

    Hide overflow, Move rightmost elements of navbar

--- src/Pages/Components/NavBarComponent.tsx
+++ src/Pages/Components/NavBarComponent.tsx
@@ -4,18 +4,23 @@ import Logo from '../../Images/Logo.png'
 
 const NavBarStyles = styled.div`
     .NavBar{
+        overflow: hidden;   
         background-color: black;
         width: 100%;
         height: 55px;
     }
 
     .NavBar a{
+        letter-spacing: 2.5px;
         color: white;
         float: right;
-        padding: 15px; 
-        padding-right: 40px;
+        padding: 16px 16px; 
         text-align: center;
         text-decoration: none;
+    }
+
+    .NavBar .rightMost {
+        margin-right: 60px;
     }
        
     #Logo {
@@ -36,15 +41,14 @@ const NavBar : React.FC = () => {
     return(
             <NavBarStyles>
                   <div className='NavBar'>
+                      <div className='rightMost'>
+                        <a href="#about"> About </a>
+                        <a href="#login"> Login </a>
+                      </div>
                       <a id="Logo" href="#landing"> <img src={Logo}/> </a>
-                      <a href="#about"> About </a>
-                      <a href="#login"> Login </a>
                   </div>
-  
         </NavBarStyles>
-
     );
-
 }
 
 export default NavBar
