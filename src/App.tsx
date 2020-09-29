a8015f09d3d032258b0e7b76b544ed4a1f4b64d1
Steven Pham <stevenpham408@yahoo.com>
2020-09-28 17:01:47 -0700

    Changed import source folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LandingPage from './Components/LandingPage'
+import LandingPage from './Pages/LandingPage'
 
 function App() {
   return (
     <div className="App">
       <LandingPage/>
-
     </div>
   );
 }
