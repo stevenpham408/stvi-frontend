fa901045999841b38bf98c913c28b0e3ea14d532
Steven Pham <stevenpham408@yahoo.com>
2020-10-03 19:20:21 -0700

    Created the main page where users can shorten URLs

---
+++ src/Pages/UserMainPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles.css'
+const UserMainPage : React.FC = () => {
+    return(
+        <body>
+            <h1> URL Shortener </h1>
+
+        </body>
+
+    )
+}
