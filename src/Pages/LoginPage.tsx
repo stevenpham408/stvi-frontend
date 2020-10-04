da2b4c88875c3d1b6033675344ccd0fe32ae5d6e
Steven Pham <stevenpham408@yahoo.com>
2020-10-03 17:25:18 -0700

    Added form functionality to login page

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -5,50 +5,89 @@ import NavBar from './Components/NavBarComponent'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'
+const initialLoginData = Object.freeze({
+    username : "",
+    password : ""
+});
 
 const LoginPage: React.FC = () => {
+    const [loginData, setLoginData] = React.useState(initialLoginData);
+    const handleChange = (e : any) => {
+        setLoginData({
+            ...loginData,
+            [e.target.name] : e.target.value
+        });
+    };
+
+    const handleLogin = (e : any) => {
+        let isEmpty = Object.values(loginData).some(element => element === "");
+        if(isEmpty){
+            alert("Fill in the missing fields.");
+            return;
+        }
+
+        axios.post("http://localhost:4040/login", loginData)
+            .then(response => {
+                console.log(response);
+                console.log("Login successful.");
+            })
+            .catch(error => {
+                console.log(error);
+                console.log("Something happened while logging in.")
+            })
+
+    }
     return (
         <>
-        <NavBar/>
-        <div className='login-text'>
-            <h1>
-                Log in to stvi
-            </h1>
-            <h2>
-                Good to see you again
-            </h2>
-        </div>
-        <div className='form-container'>
-            <Form>
-
-                <div className='form-group'>
-                    <label> Username </label>
-                    <input type="text" 
-                    name="username" 
-                    // value={formData["username"]}
-                    className="form-control" 
-                    id="registrationInputUsername" 
-                    placeholder="Username" 
-                    // onChange={handleChange}
-                    />
+            <NavBar/>
+            <div className="main-login-container">
+                <div className = "decorative-text">
+                    <h1 className='standard-text'> Let's get to
+                        <span className='colored-text'> work. </span> 
+                    </h1>
                 </div>
-    
-                <div className='form-group'>
-                    <label> Password </label>
-                    <input type="password" 
-                    // name="password" 
-                    // value={formData["password"]}
-                    className="form-control" 
-                    id="registrationInputPw" 
-                    placeholder="Password" 
-                    // onChange={handleChange}
-                    />
-                </div>
-    
                 
-                <Button type="submit" className="btn-lg btn-flat">Log in</Button>
-            </Form> 
-        </div> 
+                <div className='login-form-container'>
+                    <div className='login-text'>
+                        <h1>
+                            Log in to stvi
+                        </h1>
+                        <h2>
+                            Good to see you again
+                        </h2>
+                    </div>
+
+                    <Form>
+                        <div className='form-group'>
+                            <label> Username </label>
+                            <input type="text" 
+                            name="username" 
+                            value={loginData["username"]}
+                            className="form-control" 
+                            id="registrationInputUsername" 
+                            placeholder="Username" 
+                            onChange={handleChange}
+                            />
+                        </div>
+            
+                        <div className='form-group'>
+                            <label> Password </label>
+                            <input type="password" 
+                            name="password" 
+                            value={loginData["password"]}
+                            className="form-control" 
+                            id="registrationInputPw" 
+                            placeholder="Password" 
+                            onChange={handleChange}
+                            />
+                        </div>
+
+                        <Button type="submit" className="btn-lg btn-flat" id="login-button" onClick={handleLogin}>Log in</Button>
+                    </Form> 
+                </div> 
+            </div>
         </>
     );
 };
+
+export default LoginPage
