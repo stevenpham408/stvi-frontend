d1de8ede832fe597afda41b5350a63c137820252
Steven Pham <stevenpham408@yahoo.com>
2020-10-04 20:35:53 -0700

    Added comments for documentation

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -5,11 +5,16 @@ import NavBar from './Components/NavBarComponent'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'
+
+// Initial state for the login form, freeze it to prevent any changes from occurring
 const initialLoginData = Object.freeze({
     username : "",
     password : ""
 });
 
+/**
+ * Functional component that represents the entire Login page
+ */
 const LoginPage: React.FC = () => {
     const [loginData, setLoginData] = React.useState(initialLoginData);
     const handleChange = (e : any) => {
@@ -19,23 +24,29 @@ const LoginPage: React.FC = () => {
         });
     };
 
+    /**
+     * Function expression for handling form data after user submits the form
+     */
     const handleLogin = (e : any) => {
+        // Check for empty fields before making a request to the API
         let isEmpty = Object.values(loginData).some(element => element === "");
         if(isEmpty){
             alert("Fill in the missing fields.");
             return;
         }
 
+        // Make a post request to the API using axios
         axios.post("http://localhost:4040/login", loginData)
             .then(response => {
+                // NOTE: Need to redirect the frontend to the user main page
                 console.log(response);
                 console.log("Login successful.");
             })
+            // NOTE: Need to render a bootstrap alert that login failed
             .catch(error => {
                 console.log(error);
                 console.log("Something happened while logging in.")
             })
-
     }
     return (
         <>
--- src/Pages/RegistrationPage.tsx
+++ src/Pages/RegistrationPage.tsx
@@ -7,48 +7,76 @@ import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'
 
+// Initial state for the registration form, freeze it to prevent any changes from occurring
 const initialFormData = Object.freeze({
     email : "",
     username : "",
     password : "",
 })
+
+/**
+ * Functional component that represents the entire Registration page
+ */
 const RegistrationPage: React.FC = () => {
     const [formData, setFormData] = React.useState(initialFormData);
     const [matchingPw, setMatchingPw] = React.useState('');
+
+    /**
+     * Function expression for updating the state of the respective fields on any change  
+     */
     const handleChange = (e : any) => {
         setFormData({
             ...formData,
+
+            /* Map each <input> names to the corresponding property of formData & update its state/value 
+             * based on the value of the <input>
+             */
             [e.target.name] : e.target.value
         })
     };
 
+    /**
+     * Function expression for handling form data after user submits the form
+     */
     const handleSubmit = (e : any) => {
+        // Prevent the page from refreshing
         e.preventDefault();
         console.log(formData);
+
+        // Perform various input validation checks
         let isEmpty = Object.values(formData).some(element => element === "");
+        
+        // Check if any input fields are left blank
         if(isEmpty || matchingPw === ""){
             alert("Fill in the missing fields.");
             return;
         }
 
+        // Check if user entered a valid email
         if(!isEmail(formData["email"])){
             alert("You entered a non-valid email.");
             return;
         }
 
+        // Check if both password fields match each other
         if (formData['password'] !== matchingPw){
             alert("passwords don't match");
             return;
         }
 
+        // Make a post request to the API 
         axios.post("http://localhost:4040/registration", formData)
             .then(response => {
                 console.log(response);
+                // NOTE: Need to use Bootstrap alert
+
+                // Reset the state/value of the form fields 
                 setFormData(initialFormData);
                 setMatchingPw("");
                 alert("You have successfully created an account!")
             })
             .catch(error => {
+                // NOTE: Need to use Bootstrap alert
                 alert("Something went wrong. Please try again later.");
                 console.log(error);
             })
