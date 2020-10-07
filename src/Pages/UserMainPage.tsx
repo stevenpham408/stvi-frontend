b6f528671afd3abd854279000195097b33a3bf12
Steven Pham <stevenpham408@yahoo.com>
2020-10-07 03:43:34 -0700

    Successfully makes request to API on submission

--- src/Pages/UserMainPage.tsx
+++ src/Pages/UserMainPage.tsx
@@ -7,28 +7,74 @@ import Table from 'react-bootstrap/Table'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'
 
-const UserMainPage : React.FC = () => {
-    const [urlData, setUrlData] = React.useState('');
+function replacer(key:any,value:any)
+{
+    if (key==="pageable" || key==="sort" || key==="last" || key==="totalPages" || key==="totalElements" || key==="number" || key==="size" || key==="numberOfElements" || key==="first" || key==="empty"){
+        return undefined;
+    }
+    return value;
+}
 
+const initialUrlData = Object.freeze({
+    longUrl: ""
+});
+
+const config = Object.freeze({
+    withCredentials: true,  
+    headers: {
+      crossDomain: true, 
+      'Access-Control-Allow-Origin' : 'http://localhost:4040',
+      'Content-Type': 'application/json'
+    },
+});
+
+const UserMainPage : React.FC<urlHistoryState> = () => {
+    const [urlData, setUrlData] = React.useState(initialUrlData);
+
+    const handleChange = (e:any) => {
+        setUrlData({
+            ...urlData,
+            longUrl : e.target.value
+        })    
+    }
+
+    const urlJsonPromise = async () => {
+        const promise = axios.get("http://localhost:4040/user/url");
+        const dataPromise = promise.then((response) => response.data);
+
+        return dataPromise;
+    };
+ 
+    const handleSubmit = (e : any) => {
+        e.preventDefault();
+        axios.post("http://localhost:4040/url", urlData, config)
+        .then(async response => {
+            let content = await urlJsonPromise();
+            let parsed = JSON.parse(JSON.stringify(content, replacer))
+        })  
+        .catch(error => {
+            console.log("There was a problem processing your request." + urlData);
+        })  
+    }
     return(
-        <body>
             <div className='shortener-container'>
                 <h1> URL Shortener </h1>
                 <Form className='my-4'>
                     <div className='urlshorten-form-group'>
                         <input required
                            type="text" 
-                           value={urlData}
+                           value={urlData["longUrl"]}
                            className="urlform-control" 
                            placeholder="Input URL here" 
-                           onChange={(e) => {setUrlData(e.target.value)}}
+                           onChange={handleChange}
                         />
-                        <Button type="submit" className="btn-flat" >Shorten</Button>
-                    </div>
+                        <Button type="submit" className="btn-flat" onClick={handleSubmit} >Shorten</Button>
+                    </div> 
                 </Form>
+                
 
-                <Table className="table table-striped">
-                    <thead>
+                {/* <Table className="table table-striped">
+                    <thead className='yesthead'>
                         <tr>
                             <th> Original URL </th>
                             <th> Short URL </th>
@@ -42,11 +88,9 @@ const UserMainPage : React.FC = () => {
                             <td> 0 </td>
                         </tr>
                     </tbody>
-                </Table>
+                </Table> */}
             </div>
-        </body>
-
     )
-}
+}   
 
 export default UserMainPage
