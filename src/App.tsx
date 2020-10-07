b6ec6e748b8f99721da3d396debd713d6363146b
Steven Pham <stevenpham408@yahoo.com>
2020-10-06 17:59:54 -0700

    Configured basic routing to different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import './App.css';
-// import LandingPage from './Pages/LandingPage'
-// import RegistrationPage from './Pages/RegistrationPage'
-// import LoginPage from './Pages/LoginPage'
+import LandingPage from './Pages/LandingPage'
+import RegistrationPage from './Pages/RegistrationPage'
+import LoginPage from './Pages/LoginPage'
 import UserMainPage from './Pages/UserMainPage'
 
 function App() {
   return (
-    <div className="App">
-      {/* <LandingPage/> */}
-      {/* <RegistrationPage/> */}
-      {/* <LoginPage/> */}
-      <UserMainPage/>
-    </div>
+    <Router>
+      <div className="App">
+         <Switch>
+          <Route path={'/'} exact> <LandingPage/> </Route>
+          <Route path={'/register'}> <RegistrationPage/> </Route>
+          <Route path={'/login'}> <LoginPage/> </Route>
+          <Route path={'/userpage'}> <UserMainPage/> </Route>
+        </Switch>
+        </div>
+    </Router>
   );
 }
 
