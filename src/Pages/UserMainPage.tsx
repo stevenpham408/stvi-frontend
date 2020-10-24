50bb5039f1badcf0a65140a4ab88a3cf933fd5b7
Steven Pham <stevenpham408@yahoo.com>
2020-10-23 22:47:46 -0700

    Redesigned; added navbar; code refactoring

--- src/Pages/UserMainPage.tsx
+++ src/Pages/UserMainPage.tsx
@@ -3,6 +3,8 @@ import axios from 'axios'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table'
+import {useHistory} from 'react-router-dom'
+import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'   
 
@@ -35,12 +37,14 @@ const config = Object.freeze({
 });
 
 const UserMainPage : React.FC = () => {
+    const history = useHistory();
     const [urlData, setUrlData] = React.useState(initialUrlData);
     const [arrayUrl, setArrayUrl] = React.useState<urlJson[] | null>([])
+    axios.defaults.withCredentials = true;
 
     React.useEffect( () => {
         const fetchData = async () => {
-            let content = await urlJsonPromise();
+            let content = await getUserUrlCollection();
             let parsed = JSON.parse(JSON.stringify(content, replacer))["content"]
             setArrayUrl(parsed)
         }
@@ -52,20 +56,28 @@ const UserMainPage : React.FC = () => {
             ...urlData,
             longUrl : e.target.value
         })    
-        alert(e.target);
     }
 
-    const urlJsonPromise = async () => {
+    const handleLogout = (e: any) => {
+        axios.post("http://localhost:4040/logout", config)
+            .then((response)=> {
+                if(response.status === 200){
+                    history.push("/");
+                }
+            })        
+    }
+
+    const getUserUrlCollection = async () => {
         const promise = axios.get("http://localhost:4040/user/url", {withCredentials: true});
         const dataPromise = promise.then((response) => response.data);
         return dataPromise;
     };
  
-    const handleSubmit = (e : any) => {
+    const handleLogin = (e : any) => {
         e.preventDefault();
         axios.post("http://localhost:4040/url", urlData, config)
         .then(async response => {
-            let content = await urlJsonPromise();
+            let content = await getUserUrlCollection();
             let parsed = JSON.parse(JSON.stringify(content, replacer))["content"]
             setArrayUrl(parsed)
         })  
@@ -73,29 +85,40 @@ const UserMainPage : React.FC = () => {
             console.log("There was a problem processing your request." + urlData);
         })  
     }
+
     return(
-            <div className='shortener-container'>
-                <h1> URL Shortener </h1>
+        <div className='shortener-container'>
+            <Nav className="justify-content-center" activeKey="/home">
+                <Nav.Item className="navbar-button-item">
+                    <Nav.Link >
+                        <Button variant="outline-light" size="sm" onClick={handleLogout}>
+                            <strong>LOG OUT</strong>
+                        </Button>
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav> 
+            <div className="user-main-page-decoration">
+                <h1>Ready to cut the fluff?</h1>
+                <h2>Let's get started.</h2>
                 <Form className='my-4'>
                     <div className='urlshorten-form-group'>
                         <input required
-                           type="text" 
-                           value={urlData["longUrl"]}
-                           className="urlform-control" 
-                           placeholder="Input URL here" 
-                           onChange={handleChange}
+                            type="text" 
+                            value={urlData["longUrl"]}
+                            className="urlform-control" 
+                            placeholder="Input URL here" 
+                            onChange={handleChange}
                         />
-                        <Button type="submit" className="btn-flat" onClick={handleSubmit} >Shorten</Button>
+                        <Button type="submit" className="btn-flat btn-shorten" onClick={handleLogin} >Shorten</Button>
                     </div> 
-                </Form>
-                
+                    </Form>
+            </div>
                 <div className="url-container">
                     <Table className="table">
                         <thead>
                             <tr>
                                 <td> Short URL </td>
                                 <td> Original URL </td>
-                                <td> User ID</td>
                             </tr>
                         </thead>
                         {arrayUrl!.map((data, key) => {
@@ -105,7 +128,6 @@ const UserMainPage : React.FC = () => {
                                         <tr>
                                         <td> <a href={"http://localhost:3000/" + data.hash}>localhost:3000/{data.hash}</a> </td>
                                         <td> {data.longUrl} </td>
-                                        <td> {data.userId} </td> 
                                         </tr>
                                     } 
                                 </tbody>
