37c288cea12260d3e43d05f8a9f48ff4ca005ed6
Steven Pham <stevenpham408@yahoo.com>
2020-10-25 22:41:35 -0700

    Trivial/minor code refactoring

--- src/Pages/Components/NavBarComponent.tsx
+++ src/Pages/Components/NavBarComponent.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../styles.css'
 import Logo from '../../Images/Logo.png'
 
 const NavBar : React.FC = () => {
-    const history = useHistory();
-    const handleButtonClick = (e: any) => {
-        history.push('/login');
-    }
+
     return(
     <Nav className="justify-content-center" activeKey="/home">
         <Nav.Item>
@@ -21,7 +18,11 @@ const NavBar : React.FC = () => {
             <Nav.Link eventKey="link-1">ABOUT</Nav.Link>
         </Nav.Item>
         <Nav.Item className="navbar-button-item">
-            <Nav.Link ><Button variant="outline-light" size="sm" onClick={handleButtonClick}><strong>LOGIN</strong></Button></Nav.Link>
+            <Nav.Link >
+                <Link to={"/login"}>
+                    <Button variant="outline-light" size="sm" > <strong>LOGIN</strong> </Button>
+                </Link>
+            </Nav.Link>
         </Nav.Item>
   </Nav>
     );
