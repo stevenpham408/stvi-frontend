c00c3a85a582fe990be266601d9f03ac4bcec4fc
Steven Pham <stevenpham408@yahoo.com>
2020-11-14 03:51:28 -0800

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
-import './App.css';
 import LandingPage from './Pages/LandingPage'
 import RegistrationPage from './Pages/RegistrationPage'
 import LoginPage from './Pages/LoginPage'
